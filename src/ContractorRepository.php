23dcdd86e7800c72d9b4ecc1836564dfeaf35406
Lucas Lutz <lutzlucas03@gmail.com>
2026-05-29 21:23:31 +0200

    Ajout modif v6

--- src/ContractorRepository.php
+++ src/ContractorRepository.php
@@ -15,9 +15,15 @@ class ContractorRepository
      */
     public function save(string $fullName, string $siret, string $activity, string $taxSystem): int
     {
-        $stmt = this->pdo->prepare([
+        $stmt = $this->pdo->prepare(
             "INSERT INTO contractor (full_name, siret, activity, tax_system)
             VALUES (:fullName, :siret, :activity, :taxSystem)"
+        );
+        $stmt->execute([
+            "fullName" => $fullName,
+            "siret" => $siret,
+            "activity" => $activity,
+            "taxSystem" => $taxSystem,
         ]);
         return (int) $this->pdo->lastInsertId();
     }
@@ -27,7 +33,7 @@ class ContractorRepository
         $stmt = $this->pdo->prepare("SELECT * FROM contractor WHERE id = :id");
         $stmt->execute(["id"=>$id]);
         $row = $stmt->fetch();
-        if (!row) 
+        if (!$row) 
         {
             return null;
         }
@@ -54,7 +60,19 @@ class ContractorRepository
         $stmt = $this->pdo->query("SELECT * FROM contractor ORDER BY id");
         $contractors = [];
         foreach ($stmt as $row) {
-            $contractors[] = $this->hydrate($row);
+            $strategy = match ($row["activity"]) {
+                "bic" => new BicStrategy(),
+                "bic-vente" => new BicVenteStrategy(),
+                "bnc" => new BncStrategy(),
+            };
+            $contractors[] = new Contractor(
+                $row["full_name"],
+                $row["siret"],
+                $row["activity"],
+                $row["tax_system"],
+                $strategy,
+                (int)$row["id"],
+            );
         }
         return $contractors;
     }
